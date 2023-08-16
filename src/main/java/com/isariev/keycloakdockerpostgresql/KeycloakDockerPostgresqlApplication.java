47311727a6312048252d43e12bb642d98a5dddbe
Vladyslav <vladsw764@icloud.com>
2023-08-16 17:18:09 +0300

    initial commit

---
+++ src/main/java/com/isariev/keycloakdockerpostgresql/KeycloakDockerPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.isariev.keycloakdockerpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakDockerPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakDockerPostgresqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isariev/keycloakdockerpostgresql/KeycloakDockerPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isariev.keycloakdockerpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakDockerPostgresqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
