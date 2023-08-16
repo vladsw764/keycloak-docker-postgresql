ab32843b5eda335c97b7f75d192c5fbdb30f1e3c
Vladyslav <vladsw764@icloud.com>
2023-08-16 18:24:25 +0300

    add Order entity

---
+++ src/main/java/com/isariev/keycloakdockerpostgresql/model/Order.java
@@ -0,0 +1,28 @@
+package com.isariev.keycloakdockerpostgresql.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderName;
+
+    private BigDecimal price;
+
+    private String category;
+}
